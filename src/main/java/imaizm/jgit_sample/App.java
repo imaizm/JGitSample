e64250a745794cbffe0ff300c57331cad16772e7
imaizm <imaizm@gmail.com>
2015-11-19 19:54:44 +0900

    Gitから情報を取得するサンプルコードを追加

--- src/main/java/imaizm/jgit_sample/App.java
+++ src/main/java/imaizm/jgit_sample/App.java
@@ -1,13 +1,48 @@
 package imaizm.jgit_sample;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ListBranchCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
 /**
- * Hello world!
- *
+ * JGitSampleApp
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args ) throws IOException, GitAPIException
+	{
+		FileRepositoryBuilder builder = new FileRepositoryBuilder();
+		Repository repository =
+			builder.setGitDir(new File(".git")).readEnvironment().findGitDir().build();
+		
+		Git git = new Git(repository);
+		
+		List<Ref> branchList =
+			git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call();
+		
+		System.out.println( "[branch]" );
+		for (Ref branch : branchList) {
+			System.out.println(branch.getName());
+		}
+		
+		Iterable<RevCommit> revCommitList = 
+			git.log().all().call();
+		
+		System.out.println( "[revCommit]" );
+		for (RevCommit revCommit : revCommitList) {
+			System.out.println(
+				revCommit.getName() + " " +
+				revCommit.getAuthorIdent() + " " +
+				revCommit.getCommitterIdent() + " " + 
+				revCommit.getShortMessage());
+		}
+	}
 }
